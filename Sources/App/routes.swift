5c223dea1b2a391d5dde72b69749613a065f1edc
Ahmed Hamdy <dimo.hamdy@gmail.com>
2019-08-13 18:57:14 +0300

    Revert router to stable version

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,25 +4,16 @@ import Vapor
 public func routes(_ router: Router) throws {
     let ipContryController = IPContryController()
     
-//    router.get { req -> Future<Country> in
-//        return ipContryController.getCountry(req)
-//    }
-//
-//    router.get("ip", String.parameter) { req -> Future<Country> in
-//        let ipAdress = try req.parameters.next(String.self)
-//        return ipContryController.getCountry(req, ipAdress: ipAdress)
-//    }
-//
-//    router.get(use: ipContryController.index)
-    
-    router.get { req in
-        return "It works!"
+    router.get { req -> Future<Country> in
+        return ipContryController.getCountry(req)
     }
 
-    
-//        router.get { req -> Future<IPRange> in
-//          return IPRange.query(on: req).first().unwrap(or: Abort.init(HTTPResponseStatus.notFound))
-//    }
+    router.get("ip", String.parameter) { req -> Future<Country> in
+        let ipAdress = try req.parameters.next(String.self)
+        return ipContryController.getCountry(req, ipAdress: ipAdress)
+    }
+
+    router.get(use: ipContryController.index)
     
 }
 
