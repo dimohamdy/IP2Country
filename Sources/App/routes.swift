533e8db010e1146a2379d0c367458185d759d90d
Ahmed Hamdy <dimo.hamdy@gmail.com>
2019-08-13 17:59:06 +0300

    Add sample access to database to debug

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,16 +4,24 @@ import Vapor
 public func routes(_ router: Router) throws {
     let ipContryController = IPContryController()
     
-    router.get { req -> Future<Country> in
-        return ipContryController.getCountry(req)
+//    router.get { req -> Future<Country> in
+//        return ipContryController.getCountry(req)
+//    }
+//
+//    router.get("ip", String.parameter) { req -> Future<Country> in
+//        let ipAdress = try req.parameters.next(String.self)
+//        return ipContryController.getCountry(req, ipAdress: ipAdress)
+//    }
+//
+//    router.get(use: ipContryController.index)
+    
+    
+
+    
+        router.get { req -> Future<IPRange> in
+          return IPRange.query(on: req).first().unwrap(or: Abort.init(HTTPResponseStatus.notFound))
     }
     
-    router.get("ip", String.parameter) { req -> Future<Country> in
-        let ipAdress = try req.parameters.next(String.self)
-        return ipContryController.getCountry(req, ipAdress: ipAdress)
-    }
-    
-    router.get(use: ipContryController.index)
 }
 
 
