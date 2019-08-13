88f44deaeeb0f250f133280ca0696da1b761a624
Ahmed Hamdy <dimo.hamdy@gmail.com>
2019-08-13 16:25:11 +0300

    Remove sqlite

--- Package.swift
+++ Package.swift
@@ -2,21 +2,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "Hello",
+    name: "IP2Country",
     products: [
-        .library(name: "Hello", targets: ["App"]),
+        .library(name: "IP2Country", targets: ["App"]),
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/sqlite.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
         
     ],
     targets: [
-        .target(name: "App", dependencies: ["SQLite","FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
-
